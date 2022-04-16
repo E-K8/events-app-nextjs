9a79a9ebb67cd8bdde131b2d9e515863b27d7e60
E-K8 <ekatplat@gmail.com>
2022-04-16 21:37:03 +0100

    add button and icons, add styles for home page

--- components/events/EventList.js
+++ components/events/EventList.js
@@ -1,10 +1,12 @@
 import EventItem from './EventItem';
 
+import styles from './EventList.module.css';
+
 function EventList(props) {
   const { items } = props;
 
   return (
-    <ul>
+    <ul className={styles.list}>
       {items.map((event) => (
         <EventItem
           key={event.id}
