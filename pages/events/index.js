0f140b06c3c34a9600d5d517492a1f9afc24aeeb
E-K8 <ekatplat@gmail.com>
2022-04-19 22:32:52 +0100

    add filter form EventsSearch

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,17 @@
+import { Fragment } from 'react';
+
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/EventList';
+import EventsSearch from '../../components/events/EventsSearch';
+
 function AllEventsPage() {
+  const events = getAllEvents();
+
   return (
-    <div>
-      <h1>All Events</h1>
-    </div>
+    <Fragment>
+      <EventsSearch />
+      <EventList items={events} />
+    </Fragment>
   );
 }
 
