552142b0b5f877e5f78d417f3f4abda7c2916b83
E-K8 <ekatplat@gmail.com>
2022-05-07 21:59:57 +0100

    add head and meta data to pages statically and dynamically

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
+import Head from 'next/head';
+
 import { getFeaturedEvents } from '../helpers/api-util';
 import EventList from '../components/events/EventList';
 
 function HomePage(props) {
   return (
     <div>
+      <Head>
+        <title>Next JS events app</title>
+        <meta name='description' content='Find a lot of great IT events' />
+      </Head>
       <EventList items={props.events} />
     </div>
   );
