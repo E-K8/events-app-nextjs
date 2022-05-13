63bc5fac6547adc584ccddfa0dd3fdf98b7a5af4
E-K8 <ekatplat@gmail.com>
2022-05-13 22:24:24 +0100

    optimise metadata: entire app and entire document

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
+// this is the root app component which is rendered for every page is being displayed. Application shell
+
+import Head from 'next/head';
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Next JS events app</title>
+        <meta name='description' content='NextJS Events' />
+        <meta name='viewport' content='initial-scale=1.0, width=devide-width' />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
